96d4d5f771710698b2d3f072dcaae29607184714
ramil <ramil147258369@gmail.com>
2023-02-27 09:34:15 +0700

    deleted token's controller

--- src/tokens/tokens.module.ts
+++ src/tokens/tokens.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { TokensService } from './tokens.service';
-import { TokensController } from './tokens.controller';
 import { User } from '../users/entities/user.entity';
 import { Token } from './entities/token.entity';
 import { SequelizeModule } from '@nestjs/sequelize';
@@ -8,7 +7,7 @@ import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   imports: [JwtModule.register({}), SequelizeModule.forFeature([Token, User])],
-  controllers: [TokensController],
+  controllers: [],
   providers: [TokensService],
   exports: [TokensService],
 })
--- src/tokens/tokens.service.ts
+++ src/tokens/tokens.service.ts
@@ -1,6 +1,4 @@
 import { Injectable } from '@nestjs/common';
-import { CreateTokenDto } from './dto/create-token.dto';
-import { UpdateTokenDto } from './dto/update-token.dto';
 import { Token } from './entities/token.entity';
 import { InjectModel } from '@nestjs/sequelize';
 import { User } from '../users/entities/user.entity';
@@ -15,10 +13,6 @@ export class TokensService {
     private tokenRepository: typeof Token,
     private jwtService: JwtService,
   ) {}
-
-  validateRefreshToken(userId: number, token: string) {
-    const secret = process.env.REFRESH_SECRET;
-  }
 
   async generateTokens(user: User): Promise<TokensEntity> {
     const payload = {
@@ -43,7 +37,9 @@ export class TokensService {
   async saveRefreshToken(userId: number, refreshToken: string) {
     // todo maybe i have to hash token
     const token = await this.tokenRepository.findOne({ where: { userId } });
+    // todo maybe $set
     if (token) {
+      // await token.$set('refreshToken', refreshToken);
       await this.tokenRepository.update(
         { refreshToken },
         { where: { userId } },
@@ -55,25 +51,5 @@ export class TokensService {
 
   async deleteTokenByUserId(userId: number) {
     await this.tokenRepository.destroy({ where: { userId } });
-  }
-
-  create(createTokenDto: CreateTokenDto) {
-    return 'This action adds a new token';
-  }
-
-  findAll() {
-    return `This action returns all tokens`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} token`;
-  }
-
-  update(id: number, updateTokenDto: UpdateTokenDto) {
-    return `This action updates a #${id} token`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} token`;
   }
 }
